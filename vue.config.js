9c7b8d235942ac0494d2a837002a4fd3d126fcc2
hdq <1607332772@qq.com>
2024-05-06 15:04:44 +0800

    feat: 固定端口号

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: './'
-  // devServer: {
-  //   disableHostCheck: true // 关闭host检查
-  // }
+  publicPath: './',
+  devServer: {
+    port: 3000
+    // disableHostCheck: true // 关闭host检查
+  }
 })
